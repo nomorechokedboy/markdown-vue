881dd2ee8d551039a3174c3ef4e6b62874829b7e
starknt <1431880400@qq.com>
2023-09-28 17:30:22 +0800

    fix: error in the nuxt dev mode, close #7

--- src/module.ts
+++ src/module.ts
@@ -12,6 +12,21 @@ export default defineNuxtModule<ModuleOptions>({
         defaults: {},
         setup(options, nuxt) {
                 const resolver = createResolver(import.meta.url)
+                const opDeps = [
+                        'style-to-object',
+                        'extend',
+                        'is-buffer',
+                        'debug'
+                ]
+
+                nuxt.options.vite.optimizeDeps =
+                        nuxt.options.vite.optimizeDeps || {}
+                nuxt.options.vite.optimizeDeps.include =
+                        nuxt.options.vite.optimizeDeps.include || []
+
+                if (nuxt.options.dev) {
+                        nuxt.options.vite.optimizeDeps.include.push(...opDeps)
+                }
 
                 addComponent({
                         name: 'VueMarkdown', // name of the component to be used in vue templates
